c839aac894665aa4b8b3cbf97733c26359d9a2c1
Chernyshov-D <imchd@yandex.ru>
2022-08-09 00:06:38 +0400

    Example002_HelloUser

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Write("Введите Ваше имя: ");
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+Console.Write("Введите Ваше имя: ");
 string userName = Console.ReadLine();
 Console.Write("Привет, ");
 Console.Write(userName);
