9f5ebca97b34150f5306f1da8e05ba5ae0f8b69f
Chernyshov-D <imchd@yandex.ru>
2022-08-09 08:53:41 +0400

    add Example005_ConditionifElse

---
+++ Example005_ConditionifElse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
